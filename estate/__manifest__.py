baec4cb52717eb76a4dac1622cc7fd6e10ac0572
Waqas Ahmad <waqas.ahmad@thoughtful.ai>
2026-04-24 18:11:41 +0500

    Odoo tutorial: Chapter three Models and Basic fields.

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+
+    'summary': """
+        Starting module for "Master the Odoo web framework, chapter 1: Build a Clicker game"
+    """,
+
+    'description': """
+        Starting module for "Master the Odoo web framework, chapter 1: Build a Clicker game"
+    """,
+
+    'author': "Nighat Shabbir",
+    'category': 'Rea estate',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base', ],
+    'license': 'AGPL-3'
+}
